50a893c9d45f34c7135c0ef1a4f3a8afc685d035
github-action-benchmark <github@users.noreply.github.com>
2022-01-08 20:16:51 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 49e66914802194472a358ea67150b0e32ceb8042

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641672382086,
+  "lastUpdate": 1641673011858,
   "repoUrl": "https://github.com/LinqLover/squeak-tracedebugger",
   "entries": {
     "Benchmark": [
@@ -54,6 +54,60 @@ window.BENCHMARK_DATA = {
             "value": 2420.3333333333335,
             "unit": "Milliseconds",
             "range": 5614.333333333333
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "christoph.thiede@outlook.de",
+            "name": "Christoph Thiede",
+            "username": "LinqLover"
+          },
+          "committer": {
+            "email": "christoph.thiede@outlook.de",
+            "name": "Christoph Thiede",
+            "username": "LinqLover"
+          },
+          "distinct": true,
+          "id": "49e66914802194472a358ea67150b0e32ceb8042",
+          "message": "Re-trigger CI",
+          "timestamp": "2022-01-08T20:12:46Z",
+          "tree_id": "ebb87d0a5d4ebcfb00b9691d2047433ee8336814",
+          "url": "https://github.com/LinqLover/squeak-tracedebugger/commit/49e66914802194472a358ea67150b0e32ceb8042"
+        },
+        "date": 1641673011000,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "TDBRetracingSimulatorBenchmark>>#benchRegex",
+            "value": 6485.666666666667,
+            "unit": "Milliseconds",
+            "range": 42616.333333333336
+          },
+          {
+            "name": "TDBRetracingSimulatorBenchmark>>#benchFactorial",
+            "value": 3550.6666666666665,
+            "unit": "Milliseconds",
+            "range": 19586.333333333332
+          },
+          {
+            "name": "TraceDebuggerBenchmark>>#benchExpandAll",
+            "value": 3684.6666666666665,
+            "unit": "Milliseconds",
+            "range": 1972.3333333333333
+          },
+          {
+            "name": "TDBRetracingSimulatorBenchmark>>#benchProxyImageForm",
+            "value": 1618.3333333333333,
+            "unit": "Milliseconds",
+            "range": 2777.3333333333335
+          },
+          {
+            "name": "TraceDebuggerBenchmark>>#benchStepOver",
+            "value": 1955.6666666666667,
+            "unit": "Milliseconds",
+            "range": 4034.3333333333335
           }
         ]
       }
