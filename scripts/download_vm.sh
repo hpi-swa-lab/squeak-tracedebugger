9b87b4838adf1cb2bcbd186bf4d9786d18051934
Christoph Thiede <christoph.thiede@student.hpi.de>
2022-01-23 17:49:06 +0100

    Fix download_vm.sh

--- scripts/download_vm.sh
+++ scripts/download_vm.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
 # Download latest Trunk bundle for Linux/x64 and extract VM
 wget https://files.squeak.org/nightly/Squeak-latest-64bit-Linux-x64.tar.gz
 tar -zxvf Squeak-latest-64bit-Linux-x64.tar.gz --wildcards '*/bin'
-echo ::set-output name=vm-path::"$(realpath "Squeak*/bin/squeak")"
+echo ::set-output name=vm-path::"$(realpath Squeak*/bin/squeak)"
