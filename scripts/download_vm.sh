fc1eaab1b155bc88a24a3f1f5bdcf9b0fef3aca1
Christoph Thiede <christoph.thiede@student.hpi.de>
2022-01-23 17:43:23 +0100

    Use latest VM for smalltalkCI
    For some weird reason, older VMs handle the termination inside the EHS differently.

---
+++ scripts/download_vm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Download latest Trunk bundle for Linux/x64 and extract VM
+wget https://files.squeak.org/nightly/Squeak-latest-64bit-Linux-x64.tar.gz
+tar -zxvf Squeak-latest-64bit-Linux-x64.tar.gz --wildcards '*/bin'
+echo ::set-output name=vm-path::"$(realpath "Squeak*/bin/squeak")"
